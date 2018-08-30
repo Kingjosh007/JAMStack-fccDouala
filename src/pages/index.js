5aa689b9fc9fc2b6e7833519560542c66600a9d2
King Josaphat Chewa <kingjochewa2007@gmail.com>
2018-08-30 18:28:37 +0100

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello world</h1>
+    <p>We are <b>King007</b> and <b>Black-Code</b> from Cameroon. </p>
+    <p>This is our Gatsby Site.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
